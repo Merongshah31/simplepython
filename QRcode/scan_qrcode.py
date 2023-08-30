97825f4a40d5f685c76decd57746db68c57859b9
merongshah <120739835+Merongshah31@users.noreply.github.com>
2023-08-31 01:42:39 +0800

    Update scan_qrcode.py

--- QRcode/scan_qrcode.py
+++ QRcode/scan_qrcode.py
@@ -1,8 +1,10 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-img = Image.open('C:\\Users\\shah8\\OneDrive\\Pictures\\qrcode\\test.png')
+img = Image.open('you location file')
 
 result = decode(img)
 
 print(result)
+
+#scan qrcode
